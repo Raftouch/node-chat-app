09af89e67751d7b77a4d36b4e9b25a04fa1c9b0c
Raftouch <rafanelkin@gmail.com>
2023-09-06 21:46:51 +0200

    :sparkles: added socket messaging when joined & left

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express')
-const path = require('path') 
-const app = express()
+const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+
 require('dotenv').config()
 const port = process.env.PORT || 3000
 
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', (socket) => {
+  socket.emit('msg', 'Welcome to Chat Bar')
+  socket.broadcast.emit('msg', 'A user has joined the chat')
+  socket.on('disconnect', () => {
+    io.emit('msg', 'A user has left the chat')
+  })
+})
 
 const start = () => {
   try {
-    app.listen(port, () => console.log(`App listening on port ${port}`))
+    server.listen(port, () => console.log(`App listening on port ${port}`))
   } catch (error) {
     console.log(error)
     process.exit(1)
---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+const socket = io()
+
+socket.on('msg', (msg) => {
+  console.log(msg)
+})
